921fa3941f147643911ff7df3cbd25d42eeb7773
Abdulsalamdev <abdulsalamakinyoola@gmail.com>
2024-02-28 18:31:19 +0100

    updating

--- sdash/components/layout/tableData.tsx
+++ sdash/components/layout/tableData.tsx
@@ -52,7 +52,7 @@ export const TableData = () => {
   ];
   return (
     <div className="flex gap-[20px] pt-[20px] flex-wrap-reverse">
-      <div className="bg-white p-[30px] rounded-[15px] dark:bg-[#b8c8e7] ">
+      <div className="bg-white p-[30px] rounded-[15px] dark:bg-[#161C27]">
         <div className="flex gap-[7px] items-center  bd dark:border-b-[#252D3D] pb-[10px]">
           <Database />
           <p className="text-[22px] text-[#121212] font-medium font-roboto dark:text-white">
